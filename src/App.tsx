d66a667b6b915f369a3d195dc86a68e8ee1c547e
Your Nameferdousdevloper <eng.mfhossain@gmail.com>
2024-09-19 00:41:40 +0600

    added footer section/ferdous-1

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Navbar from './Components/Navbar';
-import Footer from './Components/Footer';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Navbar></Navbar>
-        <Footer></Footer>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <button className="btn btn-primary">Primary</button>
+        <h1>ferdous</h1>
       </header>
     </div>
   );
