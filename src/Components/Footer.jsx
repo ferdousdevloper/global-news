049990c2e1dc8961515315b23dac80b08f9a0081
Your Nameferdousdevloper <eng.mfhossain@gmail.com>
2024-09-19 01:07:14 +0600

    added footer section/ferdous-2

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,28 +3,36 @@ import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div className="bg-black">
+    <div className="bg-black text-white">
       <div>
-        <h1 className="text-center py-20 text-4xl font-bold tracking-widest">
+        <h1 className="text-center py-10 sm:py-20 text-3xl sm:text-4xl font-bold tracking-widest">
           <span className="text-[#02AA08]">Global</span> News
         </h1>
       </div>
-      <div>
-        <div className="flex items-center justify-center gap-10 max-w-[800px] mx-auto">
-          <h1 className="text-3xl">Get the Latest News Delivered Right to Your Inbox!</h1>
-          <div className="flex">
-            <input className="px-10 py-2 rounded-l-xl" type="text" placeholder="Enter your email" />{" "}
-            <button className="px-6 py-2 bg-red-600 text-white font-bold rounded-r-xl">Subscribe</button>
+      <div className="px-4">
+        <div className="flex flex-col sm:flex-row items-center justify-between gap-4 max-w-[800px] mx-auto">
+          <h1 className="text-center sm:text-left text-lg sm:text-3xl">
+            Get the Latest News Delivered Right to Your Inbox!
+          </h1>
+          <div className="flex w-full sm:w-auto">
+            <input
+              className="flex-grow px-4 py-2 rounded-l-xl w-full sm:w-auto"
+              type="text"
+              placeholder="Enter your email"
+            />
+            <button className="px-4 py-2 bg-red-600 text-white font-bold rounded-r-xl">
+              Subscribe
+            </button>
           </div>
         </div>
       </div>
       <hr className="my-10 container mx-auto" />
       <footer className="px-4">
-        <div className="container flex flex-col justify-between py-10 mx-auto space-y-8 lg:flex-row lg:space-y-0">
+        <div className="container flex flex-col space-y-8 lg:space-y-0 lg:flex-row justify-between py-10 mx-auto">
           <div className=""></div>
           <div className="grid grid-cols-2 text-sm gap-x-3 gap-y-8 lg:w-full sm:grid-cols-4">
-            <div className="space-y-3">
-              <ul className="space-y-1">
+            <div className="space-y-3 text-center">
+              <ul className="space-y-5">
                 <li>
                   <a rel="noopener noreferrer" href="#">
                     Politics
@@ -42,8 +50,8 @@ const Footer = () => {
                 </li>
               </ul>
             </div>
-            <div className="space-y-3">
-              <ul className="space-y-1">
+            <div className="space-y-3 text-center">
+              <ul className="space-y-5">
                 <li>
                   <a rel="noopener noreferrer" href="#">
                     All news
@@ -51,7 +59,7 @@ const Footer = () => {
                 </li>
                 <li>
                   <a rel="noopener noreferrer" href="#">
-                    Teach
+                    Tech
                   </a>
                 </li>
                 <li>
@@ -61,8 +69,8 @@ const Footer = () => {
                 </li>
               </ul>
             </div>
-            <div className="space-y-3">
-              <ul className="space-y-1">
+            <div className="space-y-3 text-center">
+              <ul className="space-y-5">
                 <li>
                   <a rel="noopener noreferrer" href="#">
                     About
@@ -80,19 +88,18 @@ const Footer = () => {
                 </li>
               </ul>
             </div>
-            <div className="space-y-3">
-              <div className="uppercase ">Social media</div>
-              <div className="flex justify-start space-x-3">
-              <FaFacebook />
-              <FaTwitter />
-              <FaInstagram />
+            <div className="space-y-3 text-center">
+              <div className="uppercase">Social media</div>
+              <div className="flex space-x-5 text-2xl items-center justify-center">
+                <FaFacebook />
+                <FaTwitter />
+                <FaInstagram />
               </div>
-              
             </div>
           </div>
         </div>
-        <div className="py-6 text-sm text-center ">
-          © 1968 Company Co. All rights reserved.
+        <div className="text-sm text-center py-10">
+          © 2024 Global News. All Rights Reserved (Team GAMMA)
         </div>
       </footer>
     </div>
