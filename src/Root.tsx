67966ed53ddd9a1418c5961fe1a6f05f3eb3a2c1
shahinalom9343 <cuet.shahin.eee12@gmail.com>
2024-09-18 19:54:18 -0700

    Added Errorpage at Root.tsx file also npm i react-helmet async for Shahin

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,19 +1,20 @@
-import React from 'react';
-import { createBrowserRouter } from 'react-router-dom';
-import Main from './LayOut/Main';
-import Home from './Pages/Home';
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
+import Main from "./LayOut/Main";
+import Home from "./Pages/Home";
+import ErrorPage from "./Pages/ErrorPage";
 
 const Root = createBrowserRouter([
-    {
-      path: "/",
-      element: <Main/>,
-      children: [
-        {
-            path: '/',
-            element: <Home/>
-        }
-      ]
-     
-    },
-  ]);
+  {
+    path: "/",
+    element: <Main />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
 export default Root;
