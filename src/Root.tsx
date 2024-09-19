f32dd1916f95a28073c83c4f151a3381d2634b5c
shahinalom9343 <cuet.shahin.eee12@gmail.com>
2024-09-19 05:56:53 -0700

    Added Politics news page data for Shahin

--- src/Root.tsx
+++ src/Root.tsx
@@ -5,6 +5,7 @@ import Home from "./Pages/Home";
 import ErrorPage from "./Pages/ErrorPage";
 import SignInPage from "./Pages/SignInPage";
 import RegisterPage from "./Pages/RegisterPage";
+import Politics from "./Pages/Politics";
 
 const Root = createBrowserRouter([
   {
@@ -15,6 +16,10 @@ const Root = createBrowserRouter([
       {
         path: "/",
         element: <Home />,
+      },
+      {
+        path: "/category/politics",
+        element: <Politics />,
       },
       {
         path: "/login",
