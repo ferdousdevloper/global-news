feb5288fecfd1f29351cc8d408eb8968d0f3c7dc
rnrabi <rnrabi913@gmail.com>
2024-09-22 23:21:19 +0600

    customizable news setting

---
+++ src/Components/CustomizableNews.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CustomizableNews = () => {
+    return (
+        <div>
+            This is customizable news.
+        </div>
+    );
+};
+
+export default CustomizableNews;
--- src/firebase/firebase.config.ts
+++ src/firebase/firebase.config.ts
@@ -2,14 +2,33 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 // Load environment variables from .env.local
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+//   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.REACT_APP_FIREBASE_APP_ID,
+// };
+
+
+
+
+// Rabi****************Rabi 
+
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: "AIzaSyCZHB42n_80-Rr_8LMa3Q8YkAOYM8Vk-S8",
+  authDomain: "endgame-e5c55.firebaseapp.com",
+  projectId: "endgame-e5c55",
+  storageBucket: "endgame-e5c55.appspot.com",
+  messagingSenderId: "999870863315",
+  appId: "1:999870863315:web:2d9d302b9222b569980126"
 };
+
+// Rabi *************************Rabi 
+
+
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
