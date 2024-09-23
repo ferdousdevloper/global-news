c99f401fa69681472bcf3909c88fda11f30f39fa
Marziyamujib <s03552728@gmail.com>
2024-09-23 18:33:21 +0600

    add entertainment page for tasting

--- src/Pages/Entertainment.jsx
+++ src/Pages/Entertainment.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Entertainment = () => {
     return (
         <div>
-            Entettainment page
+            Entettainment pageeeeeeeeee
         </div>
     );
 };
