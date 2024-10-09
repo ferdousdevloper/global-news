30828cbf127be2ae109fdd759f8bb87f4537155b
rnrabi <rnrabi913@gmail.com>
2024-10-09 23:12:31 +0600

    when image hover show title

--- src/Components/GalleryCard.jsx
+++ src/Components/GalleryCard.jsx
@@ -4,85 +4,151 @@ const GalleryCard = () => {
     return (
         <>
             <h2 className='h-full mt-20 text-center text-3xl font-semibold text-white py-6'>Our Gallery</h2>
-            <div className=' flex justify-between gap-3'>
+            <div className=' md:grid grid-cols-2 gap-3'>
 
                 <div>
-                    <img className='w-full' src="https://i.ibb.co/j3ktw5Z/breaking-news-screen-banner-template-260nw-2181698391.webp" alt="" />
+                    <div className='card'>
+                        <img className='w-full' src="https://i.ibb.co/j3ktw5Z/breaking-news-screen-banner-template-260nw-2181698391.webp" alt="" />
+                        <div className='card-body absolute inset-0 bg-black bg-opacity-50 opacity-0 hover:opacity-100 transition-opacity duration-300 flex items-center justify-center text-white'>
+                            this is the title here
+                        </div>
+                    </div>
 
                     <div className='grid grid-cols-3 gap-3 mb-3 mt-3'>
-                        <img className='col-span-2' src="https://i.ibb.co/qjmxGtm/istockphoto-1183338499-612x612.jpg" alt="" />
-                        <div className='space-y-1'>
-                            <div className=''>
-                                <img src="https://www.newsbangla24.com/assets/news_images/2024/09/25/Lieutenant_Tanjim_image.jpg" alt="" />
+                        <div className='card col-span-2'>
+                            <img className='' src="https://i.ibb.co/qjmxGtm/istockphoto-1183338499-612x612.jpg" alt="" />
+                            <div className='card-body absolute inset-0 bg-black bg-opacity-50 opacity-0 hover:opacity-100 transition-opacity duration-300 flex items-center justify-center text-white'>
+                                this is the title here
                             </div>
-                            <div>
+                        </div>
+                        <div className='space-y-1'>
+
+                            <div className='card'>
+                                <img src="https://www.newsbangla24.com/assets/news_images/2024/09/25/Lieutenant_Tanjim_image.jpg" alt="" />
+                                <div className='card-body absolute inset-0 bg-black bg-opacity-50 opacity-0 hover:opacity-100 transition-opacity duration-300 flex items-center justify-center text-white'>
+                                    this is the title here
+                                </div>
+                            </div>
+
+                            <div className='card'>
                                 <img src="https://i.ibb.co/qjmxGtm/istockphoto-1183338499-612x612.jpg" alt="" />
+                                <div className='card-body absolute inset-0 bg-black bg-opacity-50 opacity-0 hover:opacity-100 transition-opacity duration-300 flex items-center justify-center text-white'>
+                                    this is the title here
+                                </div>
                             </div>
                         </div>
                     </div>
 
                     <div className='grid grid-cols-3 gap-3 mb-3 mt-3'>
-                        <img className='col-span-2' src="https://www.newsbangla24.com/assets/news_images/2024/09/25/Lieutenant_Tanjim_image.jpg" alt="" />
-                        <div className='space-y-1'>
-                            <div>
-                                <img src="https://i.ibb.co/qjmxGtm/istockphoto-1183338499-612x612.jpg" alt="" />
+                        <div className='card col-span-2'>
+                            <img className='' src="https://www.newsbangla24.com/assets/news_images/2024/09/25/Lieutenant_Tanjim_image.jpg" alt="" />
+                            <div className='card-body absolute inset-0 bg-black bg-opacity-50 opacity-0 hover:opacity-100 transition-opacity duration-300 flex items-center justify-center text-white'>
+                                this is the title here
                             </div>
-                            <div>
+                        </div>
+                        <div className='space-y-1'>
+                            <div className='card'>
+                                <img src="https://i.ibb.co/qjmxGtm/istockphoto-1183338499-612x612.jpg" alt="" />
+                                <div className='card-body absolute inset-0 bg-black bg-opacity-50 opacity-0 hover:opacity-100 transition-opacity duration-300 flex items-center justify-center text-white'>
+                                    this is the title here
+                                </div>
+                            </div>
+                            <div className='card'>
                                 <img src="https://www.newsbangla24.com/assets/news_images/2024/09/25/Lieutenant_Tanjim_image.jpg" alt="" />
+                                <div className='card-body absolute inset-0 bg-black bg-opacity-50 opacity-0 hover:opacity-100 transition-opacity duration-300 flex items-center justify-center text-white'>
+                                    this is the title here
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <img className='w-full' src="https://www.newsbangla24.com/assets/news_images/2024/09/25/Lieutenant_Tanjim_image.jpg" alt="" />
+                    <div className='card'>
+                        <img className='w-full' src="https://www.newsbangla24.com/assets/news_images/2024/09/25/Lieutenant_Tanjim_image.jpg" alt="" />
+                        <div className='card-body absolute inset-0 bg-black bg-opacity-50 opacity-0 hover:opacity-100 transition-opacity duration-300 flex items-center justify-center text-white'>
+                            this is the title here
+                        </div>
+                    </div>
 
                 </div>
 
 
                 <div>
                     <div className='grid grid-cols-3 gap-3'>
-                        {/* <img src="https://i.ibb.co/qjmxGtm/istockphoto-1183338499-612x612.jpg" alt="" /> */}
                         <div className='col-span-2 space-y-3 h-full'>
-                            <div>
+                            <div className='card'>
                                 <img src="https://www.newsbangla24.com/assets/news_images/2024/09/25/Lieutenant_Tanjim_image.jpg" alt="" />
+                                <div className='card-body absolute inset-0 bg-black bg-opacity-50 opacity-0 hover:opacity-100 transition-opacity duration-300 flex items-center justify-center text-white'>
+                                    this is the title here
+                                </div>
                             </div>
-                            <div>
+                            <div className='card'>
                                 <img src="https://i.ibb.co/qjmxGtm/istockphoto-1183338499-612x612.jpg" alt="" />
+                                <div className='card-body absolute inset-0 bg-black bg-opacity-50 opacity-0 hover:opacity-100 transition-opacity duration-300 flex items-center justify-center text-white'>
+                                    this is the title here
+                                </div>
                             </div>
                         </div>
                         <div className='space-y-1 w-full h-full'>
-                            <div className='h-1/3'>
+                            <div className='h-1/3 card'>
                                 <img className='w-full h-full' src="https://www.newsbangla24.com/assets/news_images/2024/09/25/Lieutenant_Tanjim_image.jpg" alt="" />
+                                <div className='card-body absolute inset-0 bg-black bg-opacity-50 opacity-0 hover:opacity-100 transition-opacity duration-300 flex items-center justify-center text-white'>
+                                    this is the title here
+                                </div>
                             </div>
-                            <div className='h-1/3'>
+                            <div className='h-1/3 card'>
                                 <img className='w-full h-full' src="https://i.ibb.co/qjmxGtm/istockphoto-1183338499-612x612.jpg" alt="" />
+                                <div className='card-body absolute inset-0 bg-black bg-opacity-50 opacity-0 hover:opacity-100 transition-opacity duration-300 flex items-center justify-center text-white'>
+                                    this is the title here
+                                </div>
                             </div>
-                            <div className='h-1/3'>
+                            <div className='h-1/3 card'>
                                 <img className='w-full h-full' src="https://i.ibb.co/qjmxGtm/istockphoto-1183338499-612x612.jpg" alt="" />
+                                <div className='card-body absolute inset-0 bg-black bg-opacity-50 opacity-0 hover:opacity-100 transition-opacity duration-300 flex items-center justify-center text-white'>
+                                    this is the title here
+                                </div>
                             </div>
                         </div>
                     </div>
 
-                    <div className='my-3'>
+                    <div className='my-3 card'>
                         <img className='w-full' src="https://i.ibb.co/j3ktw5Z/breaking-news-screen-banner-template-260nw-2181698391.webp" alt="" />
+                        <div className='card-body absolute inset-0 bg-black bg-opacity-50 opacity-0 hover:opacity-100 transition-opacity duration-300 flex items-center justify-center text-white'>
+                            this is the title here
+                        </div>
                     </div>
 
                     <div className='grid grid-cols-3 gap-3'>
                         <div className='col-span-2 space-y-3 h-full'>
-                            <div>
+                            <div className='card'>
                                 <img src="https://www.newsbangla24.com/assets/news_images/2024/09/25/Lieutenant_Tanjim_image.jpg" alt="" />
+                                <div className='card-body absolute inset-0 bg-black bg-opacity-50 opacity-0 hover:opacity-100 transition-opacity duration-300 flex items-center justify-center text-white'>
+                                    this is the title here
+                                </div>
                             </div>
-                            <div>
+                            <div className='card'>
                                 <img src="https://i.ibb.co/j3ktw5Z/breaking-news-screen-banner-template-260nw-2181698391.webp" alt="" />
+                                <div className='card-body absolute inset-0 bg-black bg-opacity-50 opacity-0 hover:opacity-100 transition-opacity duration-300 flex items-center justify-center text-white'>
+                                    this is the title here
+                                </div>
                             </div>
                         </div>
                         <div className='space-y-1'>
-                            <div className='h-1/3'>
+                            <div className='h-1/3 card'>
                                 <img className='w-full h-full' src="https://www.newsbangla24.com/assets/news_images/2024/09/25/Lieutenant_Tanjim_image.jpg" alt="" />
+                                <div className='card-body absolute inset-0 bg-black bg-opacity-50 opacity-0 hover:opacity-100 transition-opacity duration-300 flex items-center justify-center text-white'>
+                                    this is the title here
+                                </div>
                             </div>
-                            <div className='h-1/3'>
+                            <div className='h-1/3 card'>
                                 <img className='h-full' src="https://i.ibb.co/qjmxGtm/istockphoto-1183338499-612x612.jpg" alt="" />
+                                <div className='card-body absolute inset-0 bg-black bg-opacity-50 opacity-0 hover:opacity-100 transition-opacity duration-300 flex items-center justify-center text-white'>
+                                    this is the title here
+                                </div>
                             </div>
-                            <div className='h-1/3'>
+                            <div className='h-1/3 card'>
                                 <img className='h-full' src="https://i.ibb.co/qjmxGtm/istockphoto-1183338499-612x612.jpg" alt="" />
+                                <div className='card-body absolute inset-0 bg-black bg-opacity-50 opacity-0 hover:opacity-100 transition-opacity duration-300 flex items-center justify-center text-white'>
+                                    this is the title here
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -95,3 +161,6 @@ const GalleryCard = () => {
 };
 
 export default GalleryCard;
+
+
+
